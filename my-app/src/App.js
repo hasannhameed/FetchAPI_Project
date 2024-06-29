eade8042bda89aa35084ea64398a46ab3f04f06a
hasanhameed <hasanhameed858@gmail.com>
2024-06-30 00:34:11 +0530

    fire base database used

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,22 +13,24 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-d64f8-default-rtdb.firebaseio.com/movies.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
+      const loadedMovies = [];
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -39,10 +41,40 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    try {
+      const response = await fetch('https://react-d64f8-default-rtdb.firebaseio.com/movies.json', {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to add movie.');
+      }
+
+      const data = await response.json();
+      const loadMovies = [];
+
+      for(const key in data){
+        loadMovies.push({
+          id:key,
+          title:data[key].title,
+          openingText:data[key].openingText,
+          releaseDate:data[key].releaseDate,
+        });
+      }
+
+      // Optionally re-fetch the movies or add the new movie to the local state directly
+      fetchMoviesHandler();
+    } catch (error) {
+      setError(error.message);
+    }
   }
 
+  
   let content = <p>Found no movies.</p>;
 
   if (movies.length > 0) {
